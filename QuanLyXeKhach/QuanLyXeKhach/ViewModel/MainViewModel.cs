c1d765b3ef7ec8ce2e583da379e5c0a36884c14c
zont09 <zontcn09@gmail.com>
2023-10-16 13:43:06 +0700

    Add MainViewModel, Resource

---
+++ QuanLyXeKhach/QuanLyXeKhach/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyXeKhach.ViewModel
+{
+    public class MainViewModel : QuanLyXeKach.ViewModel.BaseViewModel
+    {
+        public MainViewModel() 
+        {
+            MessageBox.Show("Dang trong main viewmodel");
+        }
+    }
+}
