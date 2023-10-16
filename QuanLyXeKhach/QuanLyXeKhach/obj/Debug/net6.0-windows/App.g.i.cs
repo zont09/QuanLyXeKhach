ee5522d36f83838219e06ec3ebf5a85e4b8d7486
zont09 <zontcn09@gmail.com>
2023-10-16 13:01:50 +0700

    Add Material Design

--- QuanLyXeKhach/QuanLyXeKhach/obj/Debug/net6.0-windows/App.g.i.cs
+++ QuanLyXeKhach/QuanLyXeKhach/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace QuanLyXeKhach {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/QuanLyXeKhach;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/QuanLyXeKhach;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
